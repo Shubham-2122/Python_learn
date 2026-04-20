d7bc95c85db95b2ed740901c13aa0b927826d3a3
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-04-20 18:48:43 +0530

    string

---
+++ Core_python/05_string/02_string_slice.py
@@ -0,0 +1,36 @@
+# index start 0
+#   01234567890123456 +
+s ="Tops Technologies"
+#   76543210987654321 -
+
+#only one index
+print(s[3])
+
+#start and end
+print(s[3:13])
+print(s[0:10])
+
+#End with
+#start with
+print(s[:7])
+print(s[5:])
+
+#start,end,jump
+print(s[0:15:3])
+
+# direct jump
+print(s[::5])
+
+#index nagetive
+print(s[-15:-2])
+print(s[:-5])
+print(s[-14:])
+print(s[-16:-4:2])
+
+#Reverse String
+print(s[::-1])
+
+#string copy
+print(s[:])
+
+
